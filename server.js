dcb0f3fb8b6d5989d03a16393741c1c8c472fb14
sergioab7 <sergioandujar7@gmail.com>
2024-04-30 19:34:10 +0200

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 import tareasRutas from "./routes/tareas.js";
 
 const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", tareasRutas);
 
